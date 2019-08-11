a6d68ba67687b869236bc468c54ae1236718cd9f
Ryan Baermann <rcbaermann@gmail.com>
2019-08-11 13:40:26 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace timeDisplay
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
